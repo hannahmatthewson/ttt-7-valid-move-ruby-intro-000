a900e24cd9f4e1ea38deea66d9cc7c19b79b30f3
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-08-07 18:28:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,10 @@
 def valid_move?(board, index)
-  if index.between?(0, 8)
+  if index.between?(0, 8) && position_taken?(board, index) == false
     return true
   else
     return false
   end
+end
   def position_taken?(board, index)
     if board[index] == " " || board[index] == ""
       return false
